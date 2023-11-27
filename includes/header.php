458f74f3892d08ac307c0f1550b8b62d6969a0f8
devsTz <rogashianmvungi@gmail.com>
2023-11-27 06:12:58 -0800

    accept changes

--- includes/header.php
+++ includes/header.php
@@ -26,19 +26,11 @@
     <div class="nav-icon"><a href="javascript:void(0)" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i></a></div>
     </div>
-<<<<<<< HEAD
     <div class="link-side">
     <a href="./index.php">Home</a>
     <a href="./about.php">About us</a>
-    <a href="">Services</a>
-    <a href="">Contact Us</a>
-=======
-    <div class="link-side nav-bar " id="myNavBar">
-   <a href="./index.php"> <i class="fa-solid fa-house font-icon"></i>&nbsp;&nbsp;Home</a>
-    <a href="./about.php"><i class="fa-regular fa-address-card font-icon "></i>&nbsp;&nbsp;About us</a>
-    <a href=""><i class="fa-solid fa-utensils font-icon"></i>&nbsp;&nbsp;Services</a>
-    <a href="./contac_us.php"><i class="fa-solid fa-phone-volume font-icon"></i>&nbsp;&nbsp;Contact Us</a>
->>>>>>> a29a6b9a8782bba5ab5439ea9227cd0b0b2ff5c9
+    <a href="./service.php">Services</a>
+    <a href="./contac_us.php">Contact Us</a>
     </div>
 
 
