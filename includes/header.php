5239c280ece407affec000ed763b24fc9063daff
ben <vivuvimalwa2000@gmail.com>
2024-01-23 13:49:01 +0300

    first added services page

--- includes/header.php
+++ includes/header.php
@@ -29,7 +29,7 @@
     <div class="link-side nav-bar " id="myNavBar">
    <a href="./index.php"> <i class="fa-solid fa-house font-icon"></i>&nbsp;&nbsp;Home</a>
     <a href="./about.php"><i class="fa-regular fa-address-card font-icon "></i>&nbsp;&nbsp;About us</a>
-    <a href=""><i class="fa-solid fa-utensils font-icon"></i>&nbsp;&nbsp;Services</a>
+    <a href="./services.php"><i class="fa-solid fa-utensils font-icon"></i>&nbsp;&nbsp;Services</a>
     <a href="./contac_us.php"><i class="fa-solid fa-phone-volume font-icon"></i>&nbsp;&nbsp;Contact Us</a>
     </div>
 
---
+++ services.php
@@ -0,0 +1,162 @@
+
+<?php require_once __DIR__ . '/includes/header.php' ?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+    <link rel="stylesheet" href="includes/css/services-css/services.css">
+    <link rel="stylesheet" href="includes/css/services-css/style.css">
+    <style>
+        [class*="col"]{
+            padding: 1rem;
+            background-color:#1c1917;
+            border:2px solid#fff;
+        }
+      
+    </style>
+  
+</head>
+<body>
+
+
+   <div class="container my-5">
+    
+    <div class="row align_items_center row1">
+      <div class="container " >
+        <div class="row align-items-center row_back" style="background-color: black;">
+            <div class="col col-style">
+                <div class="card" style="width: 18rem;">
+                    <img src="image/hero-bg.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">rice</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="image/img-3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">ugali</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+
+            </div>
+            <div class="col col-style ">
+                <div class="card" style="width: 18rem;">
+                    <img src="image/img-4.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">chicken</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+
+            </div>
+
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="image/img-2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">samaki</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+
+            </div>
+
+        </div>
+       
+      </div></div>
+
+
+
+      <div class="row align_items_center">
+       
+
+            <div class="container my-5 " >
+              <div class="row align-items-center row_back " style="background-color: black;">
+                  <div class="col " >
+                      <div class="card" style="width: 100%;">
+                          <img src="image/slide-2.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">rice</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+        
+                  </div>
+                  <div class="col ">
+                      <div class="card" style="width: 100%;">
+                          <img src="image/pizza.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">sweet pizza</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+        
+                  </div>
+                  <div class="col ">
+                      <div class="card" style="width: 100%;">
+                          <img src="image/img-4.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">chicken</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+        
+                  </div>
+        
+                  <div class="col">
+                      <div class="card" style="width: 100%;">
+                          <img src="image/hero-bg.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">samaki</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+        
+                  </div>
+        
+              </div>
+             
+            </div>
+      </div>
+
+
+    </div>
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+   
+</body>
+</html>
+<?php require_once __DIR__ . '/includes/footer.php' ?>
